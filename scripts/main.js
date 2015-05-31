53d19c1fb7a1e7341de471d6faff57885394539d
Spencer Elliott <me@elliottsj.com>
2015-05-31 12:13:52 -0400

    Enable css/stylus extraction to separate file

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,11 @@
 'use strict'
 
+// Import CSS styles
+require('../styles/main.css')
+
+// Import Stylus styles
+require('../styles/main.styl')
+
 var angular = require('angular')
 
 angular.module('HelloWorld', [
--- webpack.config.local.js
+++ webpack.config.local.js
@@ -1,5 +1,7 @@
 'use strict'
 
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+
 module.exports = {
   resolve: {
     extensions: ['', '.js', '.jsx']
@@ -10,7 +12,26 @@ module.exports = {
     publicPath: 'build/',
     filename: 'app.js'
   },
+  module: {
+    loaders: [
+      // Extract css files
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('css-loader?sourceMap')
+      },
+      // Extract stylus files
+      {
+        test: /\.styl$/,
+        loader: ExtractTextPlugin.extract('css-loader?sourceMap!stylus-loader?{sourceMap:{inline:true}}')
+      }
+    ]
+  },
+  plugins: [
+    // Extract css to app.css
+    new ExtractTextPlugin('app.css')
+  ],
   devtool: 'source-map',
   watch: true,
-  keepalive: true
+  keepalive: true,
+  failOnError: false
 }
